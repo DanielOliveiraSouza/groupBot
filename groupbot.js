272982da3ec166d6ac0865a103002e5489977b5a
DanielOliveiraSouza <oliveira.daniel109@gmail.com>
2020-10-01 00:04:19 -0300

    corrige a formatação

--- groupbot.js
+++ groupbot.js
@@ -4,21 +4,24 @@
 
 */
 var fb_spans = document.getElementsByTagName('span');
-var url_regext = new RegExp("https*");
+var url_regext = new RegExp("https:\/\/*");
 
 var filter_span = [];
-
-alert(console.dir(filter_span))
+var fp_path = '/tmp/lista.txt'
 for ( var i = 0 ; i < fb_spans.length ; i++){
 	if ( ( fb_spans[i].textContent != "" ) && ((url_regext.test(fb_spans[i].textContent) )  == true) ) {
-		filter_span.push(fb_spans[i].textContent )
+		filter_span.push(fb_spans[i].textContent + '\n\n')
 		console.log(fb_spans[i].textContent)
 
 	}
 }
 
-for (var i = 0 ; i < filter_span.length;i++){
+/*for (var i = 0 ; i < filter_span.length;i++){
 	console.log(filter_span[i])
 }
+*/
+
 //typeof(fb_spans);
 //alert(fb_spans);
+
+alert(filter_span)
