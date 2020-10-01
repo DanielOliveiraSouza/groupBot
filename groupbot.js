8c92a2e547750a508013b3e7fe3a42e6a18d2234
DanielOliveiraSouza <oliveira.daniel109@gmail.com>
2020-09-30 21:35:19 -0300

    filtra strings com a url

--- groupbot.js
+++ groupbot.js
@@ -4,11 +4,21 @@
 
 */
 var fb_spans = document.getElementsByTagName('span');
+var url_regext = new RegExp("https*");
 
+var filter_span = [];
 
+alert(console.dir(filter_span))
 for ( var i = 0 ; i < fb_spans.length ; i++){
-	console.log(fb_spans[i])
+	if ( ( fb_spans[i].textContent != "" ) && ((url_regext.test(fb_spans[i].textContent) )  == true) ) {
+		filter_span.push(fb_spans[i].textContent )
+		console.log(fb_spans[i].textContent)
+
+	}
 }
 
-typeof(fb_spans);
-alert(fb_spans);
+for (var i = 0 ; i < filter_span.length;i++){
+	console.log(filter_span[i])
+}
+//typeof(fb_spans);
+//alert(fb_spans);
