a0f03edfae1dcca04e9c385b39536f978b3b9eb4
DanielOliveiraSouza <oliveira.daniel109@gmail.com>
2020-10-06 02:55:24 -0300

    otimiza código: uso dos métodos map e filter

--- groupbot.js
+++ groupbot.js
@@ -5,36 +5,27 @@
 */
 
 
-var fb_spans = document.getElementsByTagName('span');
+var fb_spans = Array.from (new Set (document.getElementsByTagName('span')) );
 var url_regext = new RegExp("https:\/\/[A-z0-9\.#]*");
 var filter_span = [];
 var fp_path = '/tmp/lista.txt'
 var obj = null;
-
+var bercario_regex = new RegExp('Bercario+|Berçário+|berçário+|BERÇÁRIO+')
+var maternal_regex = new RegExp('MATERNAL+','i')
+var estagio2_regex = new RegExp('Estagio_II+|ESTÁGIO_II+|estágio_II+|estágio[\s]II+|ESTÁGIO[\s]II+|ESTÁGIO II+|II+')
 
 
 //
 function getFilterSpan(){
-	var bercario_regex = new RegExp('Bercario+|Berçário+|berçário+|BERÇÁRIO+')
-	var maternal_regex = new RegExp('MATERNAL+','i')
-	var estagio2_regex = new RegExp('Estagio_II+|ESTÁGIO_II+|estágio_II+|estágio[\s]II+|ESTÁGIO[\s]II+|ESTÁGIO II+|II+');
-	for ( var i = 0 ; i < fb_spans.length ; i++){
-		if ( ( fb_spans[i].textContent != "" ) && ( url_regext.test(fb_spans[i].textContent ) ) 
-			&& ! bercario_regex.test(fb_spans[i].textContent) && ! estagio2_regex.test(fb_spans[i].textContent) 
-			&& ! maternal_regex.test(fb_spans[i].textContent)){
-			filter_span.push(
-				{ 
-					id: i,
-					postContent: fb_spans[i].textContent,
-					semana:'',
-					urlPost:''
-				}
-			)
-			//filter_span.push(fb_spans[i].textContent)
-			//filter_span_str+= fb_spans[i].textContent + '\n\n'
-
-		}
-	}
+	
+	filter_span = fb_spans.map(function (mySpan,index){
+        return { id: index, urlPost:'', semana:'', postContent: mySpan.textContent }
+ 	}).filter(function(mySpan){ return ( 
+    	( 	mySpan.postContent != "" ) && ( url_regext.test(mySpan.postContent ) ) 
+				&& ! bercario_regex.test(mySpan.postContent) && ! estagio2_regex.test(mySpan.postContent) 
+				&& ! maternal_regex.test(mySpan.postContent)
+		)
+	})
 }
 
 
@@ -85,32 +76,34 @@ function getListPDFs(){
 
 }
 
+function getTagA(myElement){
+	if( myElement != null &&  myElement.tagName != 'A' ){
+		return getTagA(myElement.parentElement)
+	}else{
+		return myElement;
+	}
+}
+
+function getAttribute(myElement){
+	if ( myElement != null ){
+		return myElement.getAttribute('href')
+	}
+	return ''
+}
 
 function getUrlPost(){
-	for(i = 0; i < filter_span.length;i++){
-		var my_node = fb_spans[filter_span[i].id ]
-		var cont = 0;
-		while ( my_node != null && my_node.tagName != 'A'){
- 		   my_node = my_node.parentElement
-    		cont++;
-		}
-		if ( my_node != null ){
-			filter_span[i].urlPost = my_node.getAttribute('href')
-		}
-	}
+	filter_span = filter_span.map(function (mySpan,index){
+		 return { id: mySpan.id, urlPost:getAttribute(getTagA(fb_spans[mySpan.id])), semana:'', postContent: mySpan.postContent }
+	})
 }
 
 function main(){
 	getFilterSpan()
 	getUrlPost()
-	my_v =    Array.from ( new Set (getListVideos() ))
-	my_pdfs = Array.from ( new Set(getListPDFs()))
+	my_v =    Array.from ( new Set (getListVideos() )).reverse()
+	my_pdfs = Array.from ( new Set(getListPDFs())).reverse()
 	obj = [ my_v,my_pdfs]
 	copy(obj)
 }
 
 main()	
-
-
-
-getUrlPost()
